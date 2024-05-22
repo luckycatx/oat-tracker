e52bb27fdcf970db2032d3a1775e44a505f1fdc1
Lucky <luckycatxk@gmail.com>
2024-05-22 19:31:10 +0800

    fix: add cleanup function

--- cmd/tracker.go
+++ cmd/tracker.go
@@ -14,12 +14,11 @@ func main() {
 	var handler = handler.NewHandler(cfg)
 
 	var r = gin.Default()
-
 	// r.Use(gin.Logger())
 	// r.Use(gin.Recovery())
-
 	r.GET("/:room/announce", handler.Announce)
 	r.GET("/:room/scrape", handler.Scrape)
 
+	go handler.Cleanup()
 	log.Fatal(r.Run(cfg.Host + ":" + cfg.Port))
 }
